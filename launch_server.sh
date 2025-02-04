b118b44ece1bf3b6c88cd139aa4fb59061343fb1
Hoochi Magoochi <111466903+czechwal000@users.noreply.github.com>
2025-02-03 22:18:31 -0600

    Update launch_server.sh

--- launch_server.sh
+++ launch_server.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-# Start XVFB (X virtual framebuffer) to run the server in headless mode
-xvfb-run -a -s "-screen 0 1280x1024x24" wine /home/steam/steamcmd/icarus/icarus_server.exe -batchmode -nographics -serverName "Icarus Server"
+#!/bin/sh
+set -eu
+tini -- xvfb-run -a wine "$@"
