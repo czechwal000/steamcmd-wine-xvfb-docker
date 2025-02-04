42214e65754fde5c7a3ce46a84d3d968f7d7fb25
Hoochi Magoochi <111466903+czechwal000@users.noreply.github.com>
2025-02-03 22:22:57 -0600

    Update launch_server.sh

--- launch_server.sh
+++ launch_server.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 set -eu
-tini -- xvfb-run -a wine "$@"
+
+# Run SteamCMD to update the server before starting
+/home/steam/steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/steam/icarus/ +app_update 2089300 validate +quit
+
+# Start the server with Tini and XVFB
+tini -- xvfb-run -a wine /home/steam/icarus/icarus_server.exe -batchmode -nographics -serverName "Icarus Server"
